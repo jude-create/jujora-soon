4eab8a9f3a859d4d5edd33034fb9cd0984cd3ad2
Jude Agu <ijude236@gmail.com>
2024-04-07 00:16:39 +0100

    added favicon

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,17 @@
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import "./globals.css";
-import { metadata } from "./metadata";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-
+export const metadata = {
+  title: "Jujora",
+  description: "Connecting you directly to the heart of Agriculture. Enjoy the convenience of shopping directly from farmers, ensuring quality and authenticity every time.",
+  icons: {
+    // Replace the icon path with your desired favicon icon
+    favicon: '/favicon_io/favicon-32x32.png',
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -14,8 +20,7 @@ export default function RootLayout({ children }) {
         <meta charset="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{metadata.title}</title>
-        <link rel="icon" href={metadata.icons.icon} />
+      
       </Head>
       <body className={montserrat.className}>{children}</body>
     </html>
