c80d63ef8c5e742ee943273cdd986a072621c2d9
tEzo <tochioluebube@gmail.com>
2025-06-05 12:12:02 +0100

    corrected the testimonial carousal paritally

--- app/landing/components/testimonials.jsx
+++ app/landing/components/testimonials.jsx
@@ -13,6 +13,36 @@ export default function Testimonials() {
     "#DAFFA7",
     "#B7F2FF",
   ];
+
+  const testimonials = [
+    {
+      title:
+        "As a bachelor with a busy schedule, Jujora is a life saver. The ordering process is simple and the delivery is always on time. I've never had a bad experience with them. Highly recommend.",
+      name: "Peter",
+      location: "Lagos",
+    },
+    {
+      title:
+        "What I love the most about Jujora is their quality and consistency. I've ordered multiple times and they've never disappointed. Their customer service is friendly and helpful too. I'm definitely sticking with them.",
+      name: "Chinazam",
+      location: "Lagos",
+      color: "pink",
+    },
+    {
+      title:
+        "Jujora has completely changed the way I shop for groceries. The meat is always fresh, the produce is clean and I love how fast he delivery is. I don't stress over market runs anymore, everything comes straight to my doorstep.",
+      name: "Ada",
+      location: "Lagos",
+      color: "#DAFFA7",
+    },
+    {
+      title:
+        "Jujora has completely changed the way I shop for groceries. The meat is always fresh, the produce is clean and I love how fast he delivery is. I don't stress over market runs anymore, everything comes straight to my doorstep.",
+      name: "Tola",
+      location: "Lagos",
+      color: "#DAFFA7",
+    },
+  ];
   const itemRefs = useRef([]);
 
   const [activeIndex, setActiveIndex] = useState(0);
@@ -39,11 +69,11 @@ export default function Testimonials() {
 
         <div className="w-full flex flex-col justify-around items-center my-20 gap-20 md:gap-0 ">
           <div className="flex flex-col md:flex-row overflow-x-auto gap-10  scrollbar-hide h-max w-full ">
-            {testimonials_color.map((e, index) => (
+            {testimonials.map((testimonial, index) => (
               <div
                 key={index}
                 ref={(el) => (itemRefs.current[index] = el)}
-                className={`flex h-[80%] min-w-[400px] md:min-w-[500px] mx-auto -z-10 gap-10 items-center justify-between bg-${e} p-10 border  ${
+                className={`flex h-[80%] min-w-[400px] md:min-w-[500px] mx-auto -z-10 gap-10 items-center justify-between bg-${testimonial.color} p-10 border  ${
                   index === activeIndex
                     ? "border-4 border-orange-400  bg-white h-[90%] "
                     : "bg-green-100"
@@ -57,18 +87,15 @@ export default function Testimonials() {
                   />
                 </div>
                 <div className="flex-1 flex-col gap-5 flex ">
-                  <h1 className="text-sm">
-                    What I love the most about Jujora is their quality and
-                    consistency. I've ordered multiple times and they've never
-                    disappointed. Their customer service is friendly and helpful
-                    too. I'm definitely sticking with them.
-                  </h1>
+                  <h1 className="text-sm">{testimonial.title}</h1>
 
                   <div className="flex gap-5">
-                    <h1 className="text-primary text-2xl">Chinazam</h1>
+                    <h1 className="text-primary text-2xl">
+                      {testimonial.name}
+                    </h1>
 
                     <span className="flex justify-end items-end text-xs  ">
-                      Lagos
+                      {testimonial.location}{" "}
                     </span>
                   </div>
                 </div>
