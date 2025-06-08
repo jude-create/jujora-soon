633e6e2723e45ab4b8bc42c3e6f7c6e53bede6b1
tEzo <tochioluebube@gmail.com>
2025-06-08 17:14:33 +0100

    fixed the error and video problem

--- app/landing/components/video_section.jsx
+++ app/landing/components/video_section.jsx
@@ -1,11 +1,9 @@
 import Video from "next-video";
 import video1 from "../../../videos/video1.mp4";
-// import video2 from "../../../videos/video2.mp4.json";
 
 export default function VideoSection() {
   return (
     <section className="mt-10 md:mt-0">
-
       <div className="my-4 mx-8 flex flex-col md:flex-row gap-6 p-6 items-center justify-center aspect-video md:aspect-[1512/790]">
         <div className=" flex justify-center items-center my-auto ">
           <Video
@@ -15,12 +13,11 @@ export default function VideoSection() {
             loop
             muted
             className=" object-cover"
-          
           />
         </div>
         <div className=" flex justify-center items-center my-auto">
           <Video
-          src={video1}
+            src={video1}
             controls
             autoPlay
             loop
@@ -29,14 +26,13 @@ export default function VideoSection() {
           />
         </div>
         <div className=" flex justify-center items-center my-auto">
-          <Video
-          src={video1}
+          {/* <MuxPlayer
+            playbackId={playbackId}
+            streamType="on-demand"
             controls
             autoPlay
-            loop
-            muted
-            className=" object-cover"
-          />
+            style={{ width: "100%", height: "auto" }}
+          /> */}
         </div>
       </div>
     </section>
