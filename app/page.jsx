4af7d956960ddbf928e409aadd34de8637cdf0be
tEzo <tochioluebube@gmail.com>
2025-07-25 09:05:13 +0100

    reverted back

---
+++ app/page.jsx
@@ -0,0 +1,26 @@
+// import Navbar from "./landing/components/navbar";
+// import HeroSection from "./landing/components/hero_section";
+// import VideoSection from "./landing/components/video_section";
+// import AboutUs from "./landing/components/about_us";
+// import Testimonials from "./landing/components/testimonials";
+// import ChooseUs from "./landing/components/choose_us";
+// import Subscribe from "./landing/components/subscribe";
+// import Footer from "./landing/components/footer";
+import ComingSoon from "./ComingSoon";
+
+export default function LandingPage() {
+  return (
+    <div>
+      {/* <Navbar />
+      <HeroSection />
+      <VideoSection />
+      <AboutUs />
+      <Testimonials/>
+      <ChooseUs/>
+      <Subscribe/>
+      <Footer/> */}
+      {/* <ComingSoon/> */}
+      <ComingSoon/>
+    </div>
+  );
+}
